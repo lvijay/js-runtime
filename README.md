c999bb090c232395e74dd3e4c4bc5cfcc91f9212
Vijay Lakshminarayanan <laksvij@gmail.com>
2011-09-10 18:50:22 +0530

    Initial commit of runtime.  It works.

---
+++ README.md
@@ -0,0 +1,19 @@
+A simple JavaScript script for understanding the JavaScript runtime.
+It serves as a poor man's JavaScript runtime environment.
+
+This script runs in Mozilla's Rhino JavaScript implementation which
+can be download at [http://www.mozilla.org/rhino/download.html]().
+
+The implementation is inefficient and that's to keep it simple.  It
+uses no locks and exactly 2 primitives from the native environment:
+[java.lang.Thread#yield][1] and [java.io.PrintStream#println][2].
+
+[1]: http://download.oracle.com/javase/6/docs/api/java/lang/Thread.html#yield()
+[2]: http://download.oracle.com/javase/6/docs/api/java/io/PrintStream.html#println(java.lang.Object)
+
+Execution
+---------
+
+At the command prompt, do:
+
+    $ java -jar js.jar runtime.js
---
+++ runtime.js
@@ -0,0 +1,63 @@
+/**
+ * runtime.js - implements a simple, single-threaded JavaScript
+ * environment in JavaScript.
+ *
+ * Released in public domain.  Do what you will.  As with all other
+ * Free Software projects, this comes with NO WARRANTY.  Use at your
+ * own peril.
+ */
+var context = {};
+
+(function(who) {
+  var queue = [];
+  
+  var setTimeout = function(task, timeout) {
+    var now = new Date().getTime();
+    var end = now + timeout;
+
+    var self = function() {
+      var now = new Date().getTime();
+      if (now >= end) {
+        queue.push(task);
+      } else {
+        java.lang.Thread.currentThread().yield();
+        queue.push(self);
+      }
+    };
+
+    queue.push(self);
+  };
+
+  var executor = function(executor, queue) {
+    while (true) {
+      if (queue.length === 0) {
+        // all tasks completed
+        break;
+      }
+
+      var task = queue.shift();
+
+      if (typeof task === 'function') {
+        try { task.call(who) } catch (e) {}
+      }
+    }
+  };
+
+  who.start = function() {
+    console.log('starting...');
+    executor.call(who, executor, queue);
+  };
+
+  who.setTimeout = setTimeout;
+})(context);
+
+// Define this because Rhino doesn't
+this.console = {log: function(x) {java.lang.System.out.println(x);}};
+
+// example code
+context.setTimeout(function() {console.log('fini.');}, 10000);
+
+// start the interpreter
+context.start();
+
+/* runtime.js ends here */
